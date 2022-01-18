0aea0417b313fd518fd53faac13efc8e8361e131
JanPalmer <83087348+JanPalmer@users.noreply.github.com>
2022-01-18 12:37:07 +0100

    Add files via upload

--- lab4_1tydzien/prog1.c
+++ lab4_1tydzien/prog1.c
@@ -7,19 +7,22 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
-#define AIOBLOCKS 3
-#define BUFFERS 1
+#include <time.h>
+#define AIOBLOCKS 4
+#define BUFFERS AIOBLOCKS
 
 void error(char *);
 void usage(char *);
 void processargs(int, char**, char**, char*, int*);
-void fillaiostructs(struct aiocb*, char**, int, int);
+void fillaiostructs(struct aiocb*, char**, int*, int);
 off_t getfilelength(int);
 void readdata(struct aiocb*, off_t);
 void writedata(struct aiocb*, off_t);
 void suspend(struct aiocb*);
-void cleanup(char**, int);
+void cleanup(char**, int*);
 void processfile(struct aiocb*, char**, int);
+int processbuffer(char**, int);
+int processresults(int a, int b, int c);
 
 void error(char *msg){
     perror(msg);
@@ -43,11 +46,11 @@ void processargs(int argc, char** argv, char** filelist, char* input, int* block
 
     return;
 }
-void fillaiostructs(struct aiocb *aiocbs, char** buffer, int fd, int NumOfButes){
+void fillaiostructs(struct aiocb *aiocbs, char** buffer, int* fd, int NumOfButes){
     int i;
-    for(i = 0; i < BUFFERS; i++){
+    for(i = 0; i < AIOBLOCKS; i++){
         memset(&aiocbs[i], 0, sizeof(struct aiocb));
-        aiocbs[i].aio_fildes = fd;
+        aiocbs[i].aio_fildes = fd[i];
         aiocbs[i].aio_offset = 0;
         aiocbs[i].aio_nbytes = NumOfButes;
         aiocbs[i].aio_buf = (void*) buffer[i];
@@ -77,40 +80,95 @@ void suspend(struct aiocb* aiocbs){
     if(aio_error(aiocbs) != 0) error("Suspend error");
     if(aio_return(aiocbs) == -1) error("Return error");
 }
-void cleanup(char** buffers, int fd){
+void cleanup(char** buffers, int* fd){
     int i;
-    if(aio_cancel(fd, NULL) == -1) error("Cannot cancel async I/O");
+    for(i = 0; i < AIOBLOCKS; i++)
+        if(aio_cancel(fd[i], NULL) == -1) error("Cannot cancel async I/O");
 
     for(i = 0; i < BUFFERS; i++){
         free(buffers[i]);
     }
-    if(TEMP_FAILURE_RETRY(fsync(fd)) == -1) error("Error running fsync");
+    for(i = 0; i < AIOBLOCKS; i++)
+        if(TEMP_FAILURE_RETRY(fsync(fd[i])) == -1) error("Error running fsync");
 }
 void processfile(struct aiocb* aiocbs, char** buffer, int BufSize){
+    srand(time(NULL));
     int fileBlockNumber = getfilelength(aiocbs->aio_fildes)/BufSize;
-    int it = 0;
+    int it = 0, res, dest;
+    int IsUsed[AIOBLOCKS] = {0, 0, 0, 0};
     while(it < fileBlockNumber){
         readdata(&aiocbs[0], it*BufSize);
         suspend(&aiocbs[0]);
-        write(STDOUT_FILENO, buffer[0], BufSize);
+        res = processbuffer(buffer, BufSize);
+        if(res == 0)
+            dest = rand()%3 + 1;
+        else
+            dest = res;
+
+        if(IsUsed[dest] > 0) suspend(&aiocbs[dest]);
+
+        strcpy(buffer[dest], buffer[0]);
+        writedata(&aiocbs[dest], it*BufSize);
+        IsUsed[dest] = 1;
         it++;
     }
+
+    // initial suspend, might be redundant due to cleanup()
+    for(it = 0; it < AIOBLOCKS; it++) suspend(&aiocbs[it]);
+}
+int processbuffer(char** buffer, int BufSize){
+    int number = 0, letter = 0, other = 0;
+    for(int bi = 0; bi < BufSize; bi++){
+        // check if is a number
+        if(buffer[0][bi] >= '0' && buffer[0][bi] <= '9'){
+            number++;
+            continue;;
+        }
+
+        if((buffer[0][bi] >= 'a' && buffer[0][bi] <= 'z') || (buffer[0][bi] >= 'A' && buffer[0][bi] <= 'Z')){
+            letter++;
+            continue;;
+        }
+
+        other++;
+    }
+    return processresults(number, letter, other);
+}
+int processresults(int a, int b, int c){
+    // 0 - equal, 1 - a, 2 - b, 3 - c
+    if(a == b || a == c || b == c) return 0;
+
+    if(a > b){
+        if(a > c)
+            return 1;
+        else
+            return 3;
+    }
+    
+    if(b > c)
+        return 2;
+    else
+        return 3;
 }
 
 int main(int argc, char** argv){
     char *buffer[BUFFERS];
-    char *filenames[3], *inputfile;
-    int fdinput, blocksize;
+    char *filenames[AIOBLOCKS], *inputfile;
+    int fd[AIOBLOCKS], blocksize; // fd[0] - input, fd[1-3] - output
     struct aiocb aiocbs[AIOBLOCKS];
     processargs(argc, argv, filenames, inputfile, &blocksize);
     inputfile = argv[4];
 
-    if((fdinput = TEMP_FAILURE_RETRY(open(inputfile, O_RDONLY))) == -1) error("Cannot open input file");
-        for(int i=0; i < BUFFERS; i++) buffer[i] = (char*) calloc(blocksize, sizeof(char));
+    if((fd[0] = TEMP_FAILURE_RETRY(open(inputfile, O_RDONLY))) == -1) error("Cannot open input file");
+    for(int i=1; i < AIOBLOCKS; i++)
+        if((fd[i] = TEMP_FAILURE_RETRY(open(filenames[i-1], O_RDWR | O_TRUNC | O_CREAT, 0666))) == -1)
+            error("Cannot open output file");
 
-    fillaiostructs(aiocbs, buffer, fdinput, blocksize);
+    for(int i=0; i < BUFFERS; i++) buffer[i] = (char*) calloc(blocksize, sizeof(char));
+
+    fillaiostructs(aiocbs, buffer, fd, blocksize);
     processfile(aiocbs, buffer, blocksize);
-    cleanup(buffer, fdinput);
+    cleanup(buffer, fd);
 
     return EXIT_SUCCESS;
 }
